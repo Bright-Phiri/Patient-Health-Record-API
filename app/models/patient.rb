131af9cb65921a0830b2dba28520ccbe47461883
Bright-Phiri <bphiri.aki@gmail.com>
2021-09-20 11:12:07 +0200

    added scopes and model level validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,20 @@
 class Patient < ApplicationRecord
-  
+    has_many :vital_signs, dependent: :destroy
+    validates :first_name, :last_name, :gender, :dob, :district, :village, :occupation, presence: true
+    
+    after_validation :capitalize_names , on: [:create, :update]
+
+    default_scope {order(dob: :desc)}
+
+    scope :find_by_first_name,->(first_name){ find_by(first_name: first_name)}
+    scope :find_by_last_name,->(last_name){ find_by(last_name: last_name)}
+    scope :find_by_first_occupation,->(occupation){ find_by(occupation: occupation)}
+    scope :find_by_gender,->(gender){ find_by(gender: gender)}
+
+    private
+    def capitalize_names
+        self.first_name = first_name.capitalize
+        self.last_name = last_name.capitalize
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-   
+    has_secure_password
+    validates :username, :email, :password, :password_confirmation, presence: true
+    validates :username,:email, uniqueness: true
+    validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i ,message: ' Entered is invalid'}
+    validates :password, length: {is: 8}
+    validates :password, confirmation: true
+
+    scope :find_by_user_name,->(username){find_by!(username: username)}
+    scope :find_by_email,->(email){find_by(email: email)}
 
 end
