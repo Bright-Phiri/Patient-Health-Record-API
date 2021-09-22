06bc9f8ff760c585e90244a85f803c6e1d197bc4
Bright-Phiri <bphiri.aki@gmail.com>
2021-09-22 17:04:05 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
-      resources :patients
-      resources :vital_signs, only: [:create, :show]
+      resources :patients do
+        resources :vital_signs, only: [:create, :show]
+      end
       resources :users, only: [:index, :create]
       post '/login', to: 'users#login'
       get '/statistics',to: 'patients#statistics'
