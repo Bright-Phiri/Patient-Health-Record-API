95eb88564a80a2e264aa3bf73aa2fd0805beca0c
Bright-Phiri <bphiri.aki@gmail.com>
2021-09-29 10:39:35 +0200

    updated scopes and added custom validation for dob

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ApplicationRecord
     has_many :vital_signs, dependent: :destroy
     validates :first_name, :last_name, :gender, :dob, :district, :village, :occupation, presence: true
+    validate :date_of_birth_cannot_be_in_the_future
     
     after_validation :capitalize_names , on: [:create, :update]
 
@@ -13,6 +14,12 @@ class Patient < ApplicationRecord
     def capitalize_names
         self.first_name = first_name.capitalize
         self.last_name = last_name.capitalize
+    end
+
+    def  date_of_birth_cannot_be_in_the_future
+        if dob.present? && dob > Date.today
+            errors.add(:dob, " is Invalid")
+        end
     end
 
 end
