3a4763dae0ade43e555f1e41e115285b69034468
Bright-Phiri <bphiri.aki@gmail.com>
2021-10-21 21:06:37 +0200

    updated scopes & model level validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,9 @@
 class Patient < ApplicationRecord
     has_many :vital_signs,->{order(:created_at).reverse_order}, dependent: :destroy
-    validates :first_name, :last_name, :gender, :dob, :district, :village, presence: true
+    validates :first_name, :last_name, :dob, :district, :village, presence: true
     VALID_OCCUPATIONS = ['Accountant','Software Developer','Business Owner','Mechanic','Driver','Welder','Construction worker','Painter','Radiologist','Student','Farmer','Coal miner']
+    VALID_GENDERS = ['Male','Female']
+    validates :gender, inclusion: {in: VALID_GENDERS}
     validates :occupation, inclusion: {in: VALID_OCCUPATIONS}
     validate :date_of_birth_cannot_be_in_the_future
     
@@ -18,7 +20,7 @@ class Patient < ApplicationRecord
         self.last_name = last_name.capitalize
     end
 
-    def  date_of_birth_cannot_be_in_the_future
+    def date_of_birth_cannot_be_in_the_future
         if dob.present? && dob > Date.today
             errors.add(:dob, " is Invalid")
         end
