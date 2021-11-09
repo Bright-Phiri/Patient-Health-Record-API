9a8d5d9d1cc91bbd1e1d7fe25ebb8a47d227e9b0
Bright-Phiri <bphiri.aki@gmail.com>
2021-11-09 15:46:28 +0200

    updated scopes and model level validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, :email, :password, presence: true, uniqueness: true
+    validates :username, :email, presence: true, uniqueness: true
     validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i ,message: ' Entered is invalid'}
     validates :password, length: {is: 8}
     validates :password, confirmation: true
