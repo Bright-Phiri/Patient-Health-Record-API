2061dcd831b25c56ec1abd8144ed687157b58e88
Bright-Phiri <bphiri.aki@gmail.com>
2021-12-03 12:56:27 +0200

    added GCS service adapter & changed GCS service urls expire time

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.encoding = "utf-8"
+
 
     # Configuration for the application, engines, and railties goes here.
     #
