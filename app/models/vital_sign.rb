c18a781512f35acf0fbdbdf1bd8f3b91ee75be20
Bright-Phiri <bphiri.aki@gmail.com>
2022-01-21 10:43:17 +0200

    fixed nil values in vitals

--- app/models/vital_sign.rb
+++ app/models/vital_sign.rb
@@ -1,5 +1,5 @@
 class VitalSign < ApplicationRecord
   belongs_to :patient
-  validates :diagnosis, :weight, :height, :temp_reading, presence: true
-  validates :weight, :height, :temp_reading, numericality: true
+  validates :diagnosis, presence: true
+  validates :weight, :height, :temp_reading, numericality: true #doesn't allow nil values by default
 end
