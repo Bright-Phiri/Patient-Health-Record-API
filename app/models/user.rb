337de62dd536f842336e00ebbb6aa60efe37afef
Bright-Phiri <bphiri.aki@gmail.com>
2022-04-01 11:16:45 +0200

    using secure pass default confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
     validates :username, :email, presence: true, uniqueness: true
     validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i ,message: ' Entered is invalid'}
     validates :password, length: {in: 6..8}
-    validates :password, confirmation: true
-    validates :password_confirmation, presence: true
     scope :get_by_user_name,->(username){where(username: username).take!}
 end
